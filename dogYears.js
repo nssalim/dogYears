df9d7bc4185333727fc7e544619be336d9c3d4c4
Nargis <nargis.salim@btinternet.com>
2021-06-26 13:53:59 +0100

    first commit

---
+++ dogYears.js
@@ -0,0 +1,7 @@
+// Dog Years
+// Dogs age at a faster rate than human beings. 
+//  To convert human age from “human years” to “dog years”:
+
+// The first two years of a dog’s life count as 10.5 dog years each.
+// Each year following equates to 4 dog years.
+
